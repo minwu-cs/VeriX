021c6d3ec8912bec27c319936f7b780a44a80487
minwu-cs <minwuatuk@gmail.com>
2022-10-22 14:29:39 -0700

    facilitate execution for multiple models epsilons and inputs

--- batch.sh
+++ batch.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
 
-for i in $(seq 0 1 99)
+#  array=(0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1)
+#  for e in "${array[@]}"
+#  for e in $(seq 0.1 0.1 1)
+#  for e in $(seq 0.01 0.01 0.09)
+#  for e in $(seq 1)
+model=("mnist-10x2" "mnist-100x2" "mnist-cnn")
+epsilon=(0.05 0.1 0.5)
+for m in "${model[@]}"
 do
-  for e in "0.1"
+  for e in "${epsilon[@]}"
   do
-#    echo "$i", "$e"
-    python mnist.py --index="$i" --epsilon="$e"
+    for i in $(seq 0 1 9)
+    do
+#      echo "$m", "$e", "$i"
+      python mnist.py --network="$m" --epsilon="$e" --index="$i"
+    done
   done
 done
 
 
-#for i in $(seq 0 1 49)
+#for i in $(seq 25 1 26)
 #do
-#  for e in "0.03"
+#  for random in $(seq 0 1 19)
 #  do
-#    echo "$i", "$e"
-#    python gtsrb.py --index="$i" --epsilon="$e"
+#    python explanation_random.py --index="$i" --epsilon=0.1
 #  done
 #done
 
-echo "Done."
+#for i in $(seq 0 1 49)
+#do
+#  for e in "0.01"
+#  do
+##    echo "$i", "$e"
+#    python gtsrb.py --index="$i" --epsilon="$e"
+#  done
+#
+#done
+#
+#echo "Done."
