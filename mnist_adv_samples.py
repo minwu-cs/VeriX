1b02b3d49755989b3af0cf8a276156a7d018f558
fullmoon-cat <lixiaofu@stanford.edu>
2023-08-23 18:16:32 -0700

    changed stuff for pixel attack

--- mnist_adv_samples.py
+++ mnist_adv_samples.py
@@ -16,10 +16,11 @@ if not os.path.exists(directory):
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='mnist')
 parser.add_argument('--adv_samples', type=str, default='mnist-30x2-normal-pgd-samples')
-parser.add_argument('--network', type=str, default='mnist-10x2')
+parser.add_argument('--network', type=str, default='mnist-10x2-normal')
 parser.add_argument('--epsilon', type=float, default=0.05)
 parser.add_argument('--timeout', type=int, default=60)
 parser.add_argument('--verbosity', type=bool, default=False)
+parser.add_argument('--th', type=int, default=5)
 args = parser.parse_args()
 
 dataset = args.dataset
@@ -35,15 +36,15 @@ if not os.path.exists(output_path):
     os.mkdir(output_path)
 
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test_real, y_test) = mnist.load_data()
 # x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-# x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+x_test_real = x_test_real.reshape(x_test_real.shape[0], 28, 28, 1)
 # y_train = tf.keras.utils.to_categorical(y_train, 10)
 # Do not divide by 255 because adv samples are generated from (0, 1) clipped values already
 x_test = np.load(adv_samples_path)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)[:len(x_test)]
 # x_train = x_train.astype('float32') / 255
-# x_test = x_test.astype('float32') / 255
+x_test_real = x_test_real.astype('float32') / 255
 
 keras_model_path = directory + model_name + '.h5'
 keras_model = load_model(keras_model_path)
@@ -52,6 +53,13 @@ preds = np.argmax(logits, axis=1)
 correct_preds = np.argmax(y_test, axis=1)
 incorrect_indices = np.where(preds != correct_preds)[0]
 correct_indices = np.where(preds == correct_preds)[0]
+
+# for pixel attack, take only samples with <th pixels perturbed
+if 'pixel-attack' in args.adv_samples:
+    keep_indices = np.where(np.sum(x_test - x_test_real[:len(x_test)] != 0, axis=(1, 2)) < 6)[0]
+    incorrect_indices = np.intersect1d(keep_indices, incorrect_indices)
+    correct_indices = np.intersect1d(keep_indices, correct_indices)
+
 np.savetxt('%s%s-incorrect-indices.txt' % (output_path, model_name), incorrect_indices, fmt='%d')
 np.savetxt('%s%s-correct-indices.txt' % (output_path, model_name), correct_indices, fmt='%d')
 
